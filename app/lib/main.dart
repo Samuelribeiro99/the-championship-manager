6fc2ab3827c6600b5eef8f4df99ea53019190796
Samuel Ribeiro <samuelribeiro99@yahoo.com.br>
2025-08-07 00:31:11 -0300

    <FEAT>[Fazendo com que botões de navegação se escondam quando o app é aberto]

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'auth_page.dart';
 import 'package:app/theme/app_colors.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
